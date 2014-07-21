3642158bc0d165dd0d218d0bebd61d14a6be1ca8
Nathan Lapel <nathanlapel@gmail.com>
2014-07-21 13:04:42 +0200

    Changed the visibility of a few private virtual methods.

--- src/server/ServerSimulator.hpp
+++ src/server/ServerSimulator.hpp
@@ -20,17 +20,18 @@ class ServerSimulator : public GameSimulator
 
 	void disconnectPlayer(sf::Uint8 id, sf::Uint8 reason);
 
+	virtual void selfCastSpell(const Spell &spell);
+
+	protected:
+	virtual void onNewRoundStarted(const std::string &mapname);
+	virtual void onEntityAdded(GameEntity *entity);
+	virtual void onEntityRemoved(GameEntity *entity);
+
 	private:
 	void netThread();
 	void acceptNewConnections(std::list<std::unique_ptr<sf::TcpSocket>> &newclients, sf::SocketSelector &selector);
 	bool receivePlayerConnectionInfo(std::unique_ptr<sf::TcpSocket> &socket, sf::SocketSelector &selector); //Returns false if no information was received (true if some data was received or if the client disconnected).
 	int receiveNewPackets(sf::Uint8 id, SafeSocket<sf::TcpSocket> &socket);//Returns -1 if no error, and a DisconnectionReason otherwise
-
-	virtual void selfCastSpell(const Spell &spell);
-
-	virtual void onNewRoundStarted(const std::string &mapname);
-	virtual void onEntityAdded(GameEntity *entity);
-	virtual void onEntityRemoved(GameEntity *entity);
 
 	void acceptNewPlayer(const sf::IpAddress &address, unsigned short port, Player &toaccept);
 	void parseNewPacket(std::tuple<sf::Uint8, std::unique_ptr<sf::Packet>> &received);
